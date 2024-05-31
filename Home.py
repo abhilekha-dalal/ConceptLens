92fa9acdaf6d560de4ee43d84f5d6528a625465a
Abhilekha dalal <abhilekha19@gmail.com>
2024-05-31 15:32:38 -0500

    fixed slider

--- Home.py
+++ Home.py
@@ -14,7 +14,7 @@ with col1:
     video_file = open('video.mp4', 'rb')
     video_bytes = video_file.read()
     width = st.sidebar.slider(
-    label="Width", min_value=0, max_value=100, value=90, format="%d%%"
+    label="Width", min_value=0, max_value=100, value=100, format="%d%%"
     )
     width = max(width, 0.01)
     side = max((100 - width) / 2, 0.01)
